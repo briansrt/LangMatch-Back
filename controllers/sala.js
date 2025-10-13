165de2d46a4bd7e67f645d504d5ad594fae4c775
briansrt <brianstevenriofriotrochez@hotmail.com>
2025-10-13 14:51:58 -0500

    cambio de nombre

--- controllers/sala.js
+++ controllers/sala.js
@@ -9,8 +9,8 @@ dotenv.config();
 const Bedrockclient = new BedrockAgentRuntimeClient({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
   },
 });
 
